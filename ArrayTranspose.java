bd654369c0aa9806f4651a168a50c12c8ca3ad65
Swagat Satyak Mishra <121549363+swagat8249@users.noreply.github.com>
2023-07-14 01:49:36 +0530

    Add files via upload

---
+++ ArrayTranspose.java
@@ -0,0 +1,42 @@
+package assignment;
+
+public class ArrayTranspose {
+	public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        System.out.println("Original Matrix:");
+        displayMatrix(matrix);
+
+        transposeMatrix(matrix);
+
+        System.out.println("Transposed Matrix:");
+        displayMatrix(matrix);
+    }
+
+    private static void transposeMatrix(int[][] matrix) {
+        int n = matrix.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+
+    private static void displayMatrix(int[][] matrix) {
+        int n = matrix.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
